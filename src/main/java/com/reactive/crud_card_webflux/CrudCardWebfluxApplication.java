2e85fe65221d09f03a9c161a0d5f4e5c3f20eede
Mauricio Mancera <u20132121837@usco.edu.co>
2021-05-18 21:52:39 -0500

    estructura inicial

---
+++ src/main/java/com/reactive/crud_card_webflux/CrudCardWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.reactive.crud_card_webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudCardWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudCardWebfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reactive/crud_card_webflux/CrudCardWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactive.crud_card_webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudCardWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
